bb83dac08722c8e5aee2c14990d674887791db1a
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-08-09 19:35:53 +0400

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+apt install upgrade -y
+apt install php -y
+termux-setup-storage                                              pkg install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+apt-get update -y
+apt-get upgrade -y
+./install-nethunter-termux
+cat THX
